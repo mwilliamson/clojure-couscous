ab3806192713080282fbfd4a31a9d47f51b28607
Michael Williamson <mike@zwobble.org>
2016-07-09 21:46:00 +0100

    Extract parse-string

--- test/couscous/test/frontends/java_test.clj
+++ test/couscous/test/frontends/java_test.clj
@@ -5,30 +5,32 @@
     [couscous.frontends.java :refer [parse]]
     [couscous.frontends.java.ast :as java]))
 
+(defn- parse-string [source] (parse "" source))
+
 (deftest java-parser-tests
   (testing "parse enum"
     (is (=
           (java/enum-decl {:name "X", :fields ["A", "B", "C"]})
-          (parse "" "enum X { A, B, C }"))))
+          (parse-string "enum X { A, B, C }"))))
 
   (testing "class parsing:"
     (testing "parse empty class in default package"
       (is (=
             (java/class-decl {:name "X"})
-            (parse "" "class X {}"))))
+            (parse-string "class X {}"))))
 
     (testing "parse empty class in declared package"
       (is (=
             (java/class-decl {:name "com.example.X"})
-            (parse "" "package com.example; class X {}"))))
+            (parse-string "package com.example; class X {}"))))
 
     (testing "type parameters:"
       (testing "empty"
         (m/is
           (m/has-entry :type-params [])
-          (parse "" "class X {}")))
+          (parse-string "class X {}")))
 
       (testing "multiple invariant parameters"
         (m/is
           (m/has-entry :type-params ["T" "U"])
-          (parse "" "class X<T, U> {}"))))))
+          (parse-string "class X<T, U> {}"))))))
